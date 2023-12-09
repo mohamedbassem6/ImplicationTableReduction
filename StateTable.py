d0e8b36cc32dde32cec0c0bcfbed930c70acd44d
Mohamed Bassem <mohamedbassem@gmail.com>
2023-12-10 01:04:37 +0200

    added print_table method

--- StateTable.py
+++ StateTable.py
@@ -12,3 +12,17 @@ class StateTable:
 
     def get_output(self, state) -> list:
         return self.__output[state]
+
+    def print_table(self):
+        for i in range(len(self.__next_states)):
+            row = [chr(ord('a') + i), [chr(ord('a') + x) for x in self.__next_states[i]], self.__output[i]]
+
+            print(row[0], end="")
+            print(" | ", end="")
+
+            for x in row[1]:
+                print(f"{x} ", end="")
+            print("| ", end="")
+
+            for x in row[2]:
+                print(f"{x} ")
