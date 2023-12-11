6fdc2b2e4961800fd1a4988b0c8afd32d4045dda
Mohamed Bassem <mohamedbassem@gmail.com>
2023-12-11 13:53:11 +0200

    tested implication table population, and added more test cases from the lecture

--- Main.py
+++ Main.py
@@ -1,29 +1,55 @@
-from tables import StateTable
+from tables import StateTable, ImplicationTable
 
-table = StateTable()
+s_table = StateTable()
 
 # Test Case: https://slideplayer.com/slide/5135476/
 
-table.add([ord('b') - ord('a'), ord('c') - ord('a')], [0, 0])
-table.add([ord('d') - ord('a'), ord('e') - ord('a')], [0, 0])
-table.add([ord('f') - ord('a'), ord('g') - ord('a')], [0, 0])
-table.add([ord('h') - ord('a'), ord('i') - ord('a')], [0, 0])
-table.add([ord('j') - ord('a'), ord('k') - ord('a')], [0, 0])
-table.add([ord('l') - ord('a'), ord('m') - ord('a')], [0, 0])
-table.add([ord('n') - ord('a'), ord('o') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 1])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 1])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
-table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('b') - ord('a'), ord('c') - ord('a')], [0, 0])
+# s_table.add([ord('d') - ord('a'), ord('e') - ord('a')], [0, 0])
+# s_table.add([ord('f') - ord('a'), ord('g') - ord('a')], [0, 0])
+# s_table.add([ord('h') - ord('a'), ord('i') - ord('a')], [0, 0])
+# s_table.add([ord('j') - ord('a'), ord('k') - ord('a')], [0, 0])
+# s_table.add([ord('l') - ord('a'), ord('m') - ord('a')], [0, 0])
+# s_table.add([ord('n') - ord('a'), ord('o') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 1])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 1])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+# s_table.add([ord('a') - ord('a'), ord('a') - ord('a')], [0, 0])
+
+# Test Case: Magdy
+
+# s_table.add([ord('e') - ord('a'), ord('e') - ord('a')], [1])
+# s_table.add([ord('c') - ord('a'), ord('e') - ord('a')], [1])
+# s_table.add([ord('i') - ord('a'), ord('h') - ord('a')], [0])
+# s_table.add([ord('h') - ord('a'), ord('a') - ord('a')], [1])
+# s_table.add([ord('i') - ord('a'), ord('f') - ord('a')], [0])
+# s_table.add([ord('e') - ord('a'), ord('g') - ord('a')], [0])
+# s_table.add([ord('h') - ord('a'), ord('b') - ord('a')], [1])
+# s_table.add([ord('c') - ord('a'), ord('d') - ord('a')], [0])
+# s_table.add([ord('f') - ord('a'), ord('b') - ord('a')], [1])
+
+# s_table.add([ord('h') - ord('a'), ord('c') - ord('a')], [1, 0])
+# s_table.add([ord('c') - ord('a'), ord('d') - ord('a')], [0, 1])
+# s_table.add([ord('h') - ord('a'), ord('b') - ord('a')], [0, 0])
+# s_table.add([ord('f') - ord('a'), ord('h') - ord('a')], [0, 0])
+# s_table.add([ord('c') - ord('a'), ord('f') - ord('a')], [0, 1])
+# s_table.add([ord('f') - ord('a'), ord('g') - ord('a')], [0, 0])
+# s_table.add([ord('g') - ord('a'), ord('c') - ord('a')], [1, 0])
+# s_table.add([ord('a') - ord('a'), ord('c') - ord('a')], [1, 0])
+
 
 print("BEFORE ROW MATCHING:")
-table.print_table()
+s_table.print_table()
 
-table.row_match()
+s_table.row_match()
 
 print("AFTER ROW MATCHING:")
-table.print_table()
+s_table.print_table()
+
+i_table = ImplicationTable(s_table)
+print("IMPLICATION TABLE:")
+i_table.print_table()
