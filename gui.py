1461dab17695e3888f07f71b5d447c30ff13cdb5
Mohamed Bassem <mohamedbassem@gmail.com>
2023-12-23 20:52:54 +0200

    improved gui, and added verification for inputs

--- gui.py
+++ gui.py
@@ -8,29 +8,34 @@ class ImplicationTableReducerGUI(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.table_entries = []  # Initialize list to hold entry widgets for table data
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
+
+        self.table_entries = []
         self.title('Implication Table Reducer')
 
-        self._input_frame = tk.LabelFrame(self, borderwidth=0, labelanchor="w")
-        self._input_frame.pack(padx=10, pady=10, fill='x')
+        self._input_frame = tk.Frame(self, borderwidth=0)
+        self._input_frame.grid(row=0, column=0, sticky='nsew', padx=5, pady=10)
 
         self._states_count_entry = self.create_input(self._input_frame,
-                                                    'Enter number of states:', (10, 2), width=5)
+                                                    'Enter number of states:', width=5, row=0, col=0)
+
         self._inputs_count_entry = self.create_input(self._input_frame,
-                                                    'Enter number of inputs:', (10, 2), width=5)
+                                                    'Enter number of inputs:', width=5, row=1, col=0,)
+
         self._circuit_type_var = tk.StringVar(value='Mealy')
         self._circuit_type_entry = self.create_option_menu(self._input_frame,
                                                           'Choose circuit type:', self._circuit_type_var,
-                                                           ['Mealy', 'Moore'], (10, 2))
+                                                           ['Mealy', 'Moore'], row=2, col=0)
 
         self._update_button = tk.Button(self._input_frame, text='Update Table', command=self.update_table)
-        self._update_button.pack(anchor="w", side=tk.LEFT, padx=10)
+        self._update_button.grid(row=3, column=0, sticky='nsew')
 
-        self._table_frame = tk.LabelFrame(self, borderwidth=0)
-        self._table_frame.pack(padx=10, pady=10)
+        self._calculate_button = tk.Button(self._input_frame, text='Calculate', command=self.calculate_table_data)
+        self._calculate_button.grid(row=4, column=0, sticky='nsew')
 
-        self._calculate_button = tk.Button(self, text='Calculate', command=self.calculate_table_data)
-        self._calculate_button.pack(padx=10, pady=10)
+        self._table_frame = tk.Frame(self, borderwidth=0)
+        self._table_frame.grid(row=0, column=1, sticky='nsew', padx=5, pady=10)
 
     def get_input_count(self):
         return int(self._inputs_count_entry.get())
@@ -41,81 +46,98 @@ class ImplicationTableReducerGUI(tk.Tk):
     def get_circuit_type(self):
         return self._circuit_type_var.get()
 
-    def create_input(self, parent, label_text, pad, **entry_options):
-        label = tk.Label(parent, text=label_text)
-        label.pack(anchor="w", side=tk.LEFT, padx=pad)
-        entry = tk.Entry(parent, **entry_options)
-        entry.pack(anchor="w", side=tk.LEFT, padx=(2, 10))
+    def create_input(self, parent, label_text, row, col, **entry_options):
+        frame = tk.Frame(parent, borderwidth=0)
+        frame.grid(row=row, column=col, sticky='nsew', pady=5)
+
+        label = tk.Label(frame, text=label_text)
+        label.grid(row=0, column=0, sticky='nsew')
+
+        entry = tk.Entry(frame, **entry_options)
+        entry.grid(row=0, column=1, sticky='nsew')
+
         return entry
 
-    def create_option_menu(self, parent, label_text, variable, options, pad):
-        label = tk.Label(parent, text=label_text)
-        label.pack(anchor="w", side=tk.LEFT, padx=pad)
-        option_menu = tk.OptionMenu(parent, variable, *options)
-        option_menu.pack(anchor="w", side=tk.LEFT, padx=(2, 10))
+    def create_option_menu(self, parent, label_text, variable, options, row, col):
+        frame = tk.Frame(parent, borderwidth=0)
+        frame.grid(row=row, column=col, sticky='nsew', pady=5)
+
+        label = tk.Label(frame, text=label_text)
+        label.grid(row=0, column=0, sticky='nsew')
+
+        option_menu = tk.OptionMenu(frame, variable, *options)
+        option_menu.grid(row=0, column=1, sticky='nsew')
+
         return option_menu
 
     def calculate_table_data(self):
-        table_data = [[entry.get() for entry in row] for row in self.table_entries]
+        try:
+            table_data = [[entry.get() for entry in row] for row in self.table_entries]
 
-        input_count = app.get_input_count()
+            input_count = self.get_input_count()
 
-        s_table = StateTable()
+            s_table = StateTable()
 
-        for row in table_data:
-            next_states = row[:input_count]
-            outputs = row[input_count:]
+            for row in table_data:
+                next_states = row[:input_count]
+                for next_state in next_states:
+                    if next_state == '' or ord(next_state) - ord('a') >= self.get_states_count():
+                        raise ValueError
 
-            s_table.add([ord(x.lower()) - ord('a') for x in next_states], [ord(x) - ord('0') for x in outputs])
+                outputs = row[input_count:]
+                for output in outputs:
+                    if output == '' or output not in ['0', '1']:
+                        raise ValueError
 
-        s_table.row_match()
+                s_table.add([ord(x.lower()) - ord('a') for x in next_states], [ord(x) - ord('0') for x in outputs])
 
-        print("AFTER ROW MATCHING:")
-        s_table.print_table()
-        
-        i_table = ImplicationTable(s_table)
+            s_table.row_match()
 
-        print("IMPLICATION TABLE:")
-        i_table.print_table()
-        
-        i_table.imply()
+            print("AFTER ROW MATCHING:")
+            s_table.print_table()
 
-        print("IMPLICATION TABLE AFTER REDUCTION:")
-        i_table.print_table()
+            i_table = ImplicationTable(s_table)
 
-        print("REMAINING STATES:")
+            print("\nIMPLICATION TABLE:")
+            i_table.print_table()
 
-        remaining_states = i_table.get_remaining_states()
-        for state in remaining_states:
-            print("{", end=" ")
-            for x in state:
-                print(chr(ord('a') + x), end=" ")
-            print("}", end=" ")
-        print()
+            i_table.imply()
 
-        print("\nCLASSIFICATION:")
-        classes = i_table.classify()
+            print("IMPLICATION TABLE AFTER REDUCTION:")
+            i_table.print_table()
 
-        for c in classes:
-            print("{", end=" ")
+            print("REMAINING STATES:")
 
-            for x in c:
-                print(chr(ord('a') + x), end=" ")
+            remaining_states = i_table.get_remaining_states()
+            for state in remaining_states:
+                print("{", end=" ")
+                for x in state:
+                    print(chr(ord('a') + x), end=" ")
+                print("}", end=" ")
+            print()
 
-            print("}", end=" ")
+            print("\nCLASSIFICATION:")
+            classes = i_table.classify()
 
-        print()
+            for c in classes:
+                print("{", end=" ")
 
-        reduced_table = i_table.get_reduced_table(classes)
+                for x in c:
+                    print(chr(ord('a') + x), end=" ")
 
-        # reduced_table.print_table()
+                print("}", end=" ")
 
-        self.display_table(reduced_table.get_table_as_list())
+            print()
+
+            reduced_table = i_table.get_reduced_table(classes)
+
+            # reduced_table.print_table()
+
+            self.display_table(reduced_table.get_table_as_list())
+        except ValueError:
+            messagebox.showerror('Error', 'Recheck the provided state table.')
 
     def display_table(self, data_array):
-        """
-        Takes a 2D array and opens a new window to display it as a table.
-        """
         table_window = tk.Toplevel(self, padx=10, pady=10)
         table_window.title('Reduced Table')
 
@@ -176,8 +198,15 @@ class ImplicationTableReducerGUI(tk.Tk):
 
     def update_table(self):
         try:
-            states = int(self._states_count_entry.get())
-            inputs = int(self._inputs_count_entry.get())
+            states_entry = self._states_count_entry.get()
+            inputs_entry = self._inputs_count_entry.get()
+
+            if states_entry == '' or inputs_entry == '' or not states_entry.isdigit() or not inputs_entry.isdigit():
+                raise TypeError
+
+            states = int(states_entry)
+            inputs = int(inputs_entry)
+
             if states <= 0 or inputs <= 0:
                 raise ValueError
 
@@ -185,7 +214,10 @@ class ImplicationTableReducerGUI(tk.Tk):
             self._build_table()
 
         except ValueError:
-            messagebox.showerror('Error', 'Please enter a positive integer')
+            messagebox.showerror('Error', 'Please provide positive integer values for the number of states and inputs.')
+
+        except TypeError:
+            messagebox.showerror('Error', 'Illegal format for number of states and inputs.')
 
     def _clear_table(self):
         for widgets in self._table_frame.winfo_children():
